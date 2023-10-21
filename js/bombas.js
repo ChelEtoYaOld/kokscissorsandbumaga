4b44e141b1be1c8210ff9327514aa4e7cddc9371
ChelEtoYaOld <mishakharchenko2008@gmail.com>
2023-10-21 12:32:33 +0200

    Bumaga commit

--- js/bombas.js
+++ js/bombas.js
@@ -1,1 +1,19 @@
-console.log("Test")
+const random = (min,max) => Math.floor(min + Math.random() * (max + 1 - min))
+const aiImg = document.querySelector('.ai-choice-main img')
+const userImg = document.querySelector('.user-choice-main img')
+const userChoiceItem = document.querySelectorAll('.choice-item')
+let interval = null
+const opt = {
+    turnsAi: [],
+    turnsUser: [],
+    interval: 20,
+    userScore: 0,
+    aiScore: 0,
+}
+
+const generateAi = () => {
+    let rand = random(1,3)
+    aiImg.setAttribute('src',`./kartinki/${rand}.png`)
+}
+
+interval = setInterval(generateAi, opt.interval)
